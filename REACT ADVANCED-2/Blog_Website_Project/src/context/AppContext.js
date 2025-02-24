9b4565b594f68fcf1f63e9c364578285d8a4dac6
AMAN SINGH <amansinghbhumihar2510@gmail.com>
2025-02-24 21:52:55 +0530

    Added Blogs logics

---
+++ REACT ADVANCED-2/Blog_Website_Project/src/context/AppContext.js
@@ -0,0 +1,70 @@
+import { createContext, useState } from "react";
+import { baseUrl } from "../baseUrl";
+
+
+//Step-1
+export const AppContext = createContext();
+
+
+// Step-2
+export default function AppContextProvider({ children }) {
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
+
+    // Step-4 data filling
+
+    async function fetchBlogPosts(page = 1) {
+        setLoading(true);
+
+        let url = `${baseUrl}?page=${page}`;
+        try {
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data);
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages);
+        }
+        catch (error) {
+            alert('Error')
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null);
+        }
+        setLoading(false);
+    }
+
+
+    // Step-5
+
+    function handlePageChange(page) {
+        setPage(page);
+        fetchBlogPosts(page);
+    }
+
+
+
+    // Step-2
+    const value = {
+        posts,
+        setPosts,
+        loading,
+        setLoading,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages,
+        fetchBlogPosts,
+        handlePageChange,
+    };
+
+
+
+
+    // Step-3
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
